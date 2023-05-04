75a863a8084f5f0148e13f973c1d80247a8b4569
MASKARA <70981979+MASKAR4@users.noreply.github.com>
2023-05-04 12:48:17 -0500

    Update DatosDim3.py

--- DatosDim3.py
+++ DatosDim3.py
@@ -1,89 +1,90 @@
+import random
+
+
 class DatosDim3():
-    def __init__(self, x, y, z, valorDefecto):
-        self.datos = []
-        for i in range(x):
-            listaTmp = []
-            for j in range(y):
-                listaTmpTmp = []
-                for k in range(z):
-                    listaTmpTmp.append(valorDefecto)
-                listaTmp.append(listaTmpTmp)
-            self.datos.append(listaTmp)
 
-    def ver(self):
-        i = 0
-        for itemI in self.datos:
-            j = 0
-            for itemJ in itemI:
-                k = 0
-                for itemK in itemJ:
-                    print(f'[{i}][{j}][{k}] {itemK} \t', end='')
-                    k += 1
-                print()
-                j += 1
+    def __init__(self, filas, col, capas):
+        self.datos = [[[random.randint(0, 1000) for k in range(col)] for j in range(filas)] for i in range(capas)]
+
+
+    def verMatriz(self):
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                print("")
+                for k in range(len(self.datos[i][j])):
+                    print("[{}]".format(self.datos[i][j][k]), end="")
             print()
-            i += 1
 
-    def sumar(self):
-        acumular = 0
-        for itemI in self.datos:
-            for itemJ in itemI:
-                for itemK in itemJ:
-                    acumular = acumular + itemK
-        # print(f'Suma: {acumular}')
-        return acumular
-
-    def cambiarValor(self, x, y, z, nuevoValor):
-        try:
-            self.datos[x][y][z] = nuevoValor
-            print(f' datos[{x}][{y}][{z}]:{nuevoValor}  con Exito!')
-        except:
-            print(f' datos[{x}][{y}][{z}]:{nuevoValor}  ERROR!')
+    def suma(self):
+        acum = 0
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                for k in range(len(self.datos[i][j])):
+                    acum += self.datos[i][j][k]
+        return acum
 
     def resta(self):
-        acumular = 0
-        for itemI in self.datos:
-            for itemJ in itemI:
-                for itemK in itemJ:
-                    acumular = acumular - itemK
-        return acumular
-
-    def sumaXfilas(self):
-        suma_filas = []
+        acum = 0
         for i in range(len(self.datos)):
-            suma = 0
             for j in range(len(self.datos[i])):
                 for k in range(len(self.datos[i][j])):
-                    suma += self.datos[i][j][k]
-            suma_filas.append(suma)
-        return suma_filas
+                    acum -= self.datos[i][j][k]
+        return acum
 
-    def sumaXcolumnas(self):
-        suma_columnas = []
-        for j in range(len(self.datos[0])):
-            suma = 0
-            for i in range(len(self.datos)):
+    def sumaXCapas(self):
+        resul = [0] * len(self.datos)
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
                 for k in range(len(self.datos[i][j])):
-                    suma += self.datos[i][j][k]
-            suma_columnas.append(suma)
-        return suma_columnas
+                    resul[i] += self.datos[i][j][k]
+        return resul
 
-    def sumaXcapas(self):
-        suma_capas = []
-        for k in range(len(self.datos[0][0])):
-            suma = 0
-            for i in range(len(self.datos)):
-                for j in range(len(self.datos[i])):
-                    suma += self.datos[i][j][k]
-            suma_capas.append(suma)
-        return suma_capas
+    def sumaXFilas(self):
+        resultado = []
+        for capas in range(len(self.datos)):
+            capaActual = []
+            for filas in range(len(self.datos[capas])):
+                sumaFila = 0
+                for columnas in range(len(self.datos[capas][filas])):
+                    sumaFila += self.datos[capas][filas][columnas]
+                capaActual.append(sumaFila)
+            resultado.append(capaActual)
+        return resultado
+
+    def sumaXColumnas(self):
+        resultado = []
+        for capas in range(len(self.datos)):
+            capaActual = []
+            for columnas in range(len(self.datos[capas][0])):
+                sumaColumna = 0
+                for filas in range(len(self.datos[capas])):
+                    sumaColumna += self.datos[capas][filas][columnas]
+                capaActual.append(sumaColumna)
+            resultado.append(capaActual)
+        return resultado
 
     def verBorde(self):
-        for i in range(len(self.datos)):
-            for j in range(len(self.datos[i])):
-                for k in range(len(self.datos[i][j])):
-                    if i == 0 or i == len(self.datos) - 1 or j == 0 or j == len(
-                            self.datos[i]) - 1 or k == 0 or k == len(self.datos[i][j]) - 1:
-                        print(f'[{i}][{j}][{k}] {self.datos[i][j][k]} \t', end='')
-                    else:
-                        print('\t', end='')
+        capa = self.datos
+        numFilas = len(capa)
+        numCol = len(capa[0])
+        arregloDosDim = []
+        posiciones = []
+        for columnas in range(numCol):
+            arregloDosDim.append(capa[0][columnas])
+            posiciones.append((0, columnas))
+        for filas in range(1, numFilas):
+            arregloDosDim.append(capa[filas][numCol - 1])
+            posiciones.append((filas, numCol - 1))
+        for columnas in range(numCol - 2, -1, -1):
+            arregloDosDim.append(capa[numFilas - 1][columnas])
+            posiciones.append((numFilas - 1, columnas))
+        for filas in range(numFilas - 2, 0, -1):
+            arregloDosDim.append(capa[filas][0])
+            posiciones.append((filas, 0))
+
+        print("Arreglo: ", arregloDosDim)
+        print("Posiciones: ", posiciones)
+        return arregloDosDim
+
+   
+
