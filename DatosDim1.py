11ab88188e8301f5c9d7addc340f5c4cdb0c5dd7
MASKARA <70981979+MASKAR4@users.noreply.github.com>
2023-05-04 12:50:01 -0500

    Update DatosDim1.py

--- DatosDim1.py
+++ DatosDim1.py
@@ -1,11 +1,34 @@
-class DatosDim1():
-    def __init__(self, n, valorDefecto):
-        self.datos = []
-        for i in range(n):
-            self.datos.append(valorDefecto)
+import random
 
-    def ver(self):
-        i = 0
-        for item in self.datos:
-            print(f'[{i}] {item} \t', end='')
-            i += 1
+
+class DatosDim1():
+
+    def __init__(self, filas):
+        self.datos = [[random.randint(2, 1000) for j in range(filas)]]
+
+    def verMatriz(self):
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                print("[{}]".format(self.datos[i][j]), end="")
+            print()
+
+    def resta(self):
+        acum = 0
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                acum -= self.datos[i][j]
+        return acum
+
+    def suma(self):
+        acum = 0
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                acum += self.datos[i][j]
+        return acum
+
+    def sumaXFilas(self):
+        resul = [0] * len(self.datos)
+        for i in range(len(self.datos)):
+            for j in range(len(self.datos[i])):
+                resul[i] += self.datos[i][j]
+        return resul
